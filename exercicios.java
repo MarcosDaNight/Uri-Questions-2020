0392499f3f6ee24bd9c26b07dee487da5df23232
MarcosDaNight <53832367+MarcosDaNight@users.noreply.github.com>
2020-01-28 13:11:36 -0300

    Add files via upload

---
+++ exercicios.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class exercicios {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//06
+		
+		//double a, b, c, pi = 3.14159, triangulo, circulo, trapezio, quadrado, retangulo;
+		
+		//a = sc.nextDouble();
+		//b = sc.nextDouble();
+		//c = sc.nextDouble();
+	
+		//triangulo = (a*b)/2;
+		//circulo = c * pi;
+		//trapezio = (a * b * c)/2;
+		//quadrado = b*b;
+		//retangulo = a*b;
+		
+		//System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		//System.out.printf("CIRCULO: %.3f%n", circulo);
+		//System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		//System.out.printf("QUADRADO: %.3f%n", quadrado);
+		//System.out.printf("RETANGULO: %.3f%n", retangulo);
+	
+		
+		//03
+		//int A, B, C, D, diferencia;
+		
+		//A = sc.nextInt();
+		//B =	sc.nextInt();
+		//C = sc.nextInt();
+		//D = sc.nextInt();
+		
+		//diferencia = A * B - C * D;
+		
+		//System.out.printf("DIFERENCA = %d%n", diferencia);
+		
+		
+		//04
+		//int num, hora;
+		//double valor, salario;
+		
+		//num = sc.nextInt();
+		//hora = sc.nextInt();
+		//valor = sc.nextDouble();
+		
+		//salario = hora * valor;
+		
+		//System.out.printf("NUMBER = %d%n", num);
+		//System.out.printf("SALARY = U$ %.2f%n", salario);
+	
+			
+		sc.close();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ uri1004.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class uri1004 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y, prod;
+		
+		x = sc.nextInt();
+		
+		y = sc.nextInt();
+		
+		prod = x * y;
+		
+		System.out.println("PROD = " + prod);
+		
+		
+	}
+
+}
